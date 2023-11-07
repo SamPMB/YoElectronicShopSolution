ad432ec97b3c9d180371102c938067a5502a5159
SamPMB <samsonmumba688@gmail.com>
2023-11-07 16:52:34 +0200

    none

---
+++ YoElectronicShop/Pages/ElectronicsBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using YoElectronicShop.Models.DTO;
+using YoElectronicShop.Services.Contracts;
+
+namespace YoElectronicShop.Pages
+{
+    public class ElectronicsBase:ComponentBase
+    {
+        [Inject]
+        IProductService ProductService {  get; set; }
+
+        public IEnumerable<ProductDto> Products { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+          Products = await  ProductService.GetItems();
+        }
+
+    }
+}
